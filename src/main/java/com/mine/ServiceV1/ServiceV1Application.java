f20cac408155a52ae4961d3b4da2170829c271d8
Parveen Rawat <parveen.r21@gmail.com>
2020-06-18 11:32:04 +0530

    added fikes

---
+++ src/main/java/com/mine/ServiceV1/ServiceV1Application.java
@@ -0,0 +1,13 @@
+package com.mine.ServiceV1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceV1Application.class, args);
+	}
+
+}
